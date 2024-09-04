dba098df516b2b19771bde11c8745b678f266cd7
Tino Koch <17991193+Tinoooo@users.noreply.github.com>
2024-09-04 20:44:28 +0200

    added more props to native pixelation effect

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -9,3 +9,10 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+const orginalWarn = console.warn
+console.warn = (...args: any[]) => {
+  if (!args[0].includes('Component is missing template or render function')) {
+    orginalWarn.apply(console, args)
+  }
+}
--- src/core/three/Pixelation.vue
+++ src/core/three/Pixelation.vue
@@ -1,10 +1,14 @@
 <script lang="ts">
 import { RenderPixelatedPass } from 'three/examples/jsm/postprocessing/RenderPixelatedPass.js'
 import { useTresContext } from '@tresjs/core'
+import { watchEffect } from 'vue'
 import { useEffectThree } from '../composables/useEffectThree'
+import { makePropWatchers } from '../../util/prop'
 
 export interface PixelationProps {
   pixelSize: number
+  depthEdgeStrength?: number
+  normalEdgeStrength?: number
 }
 </script>
 
@@ -16,4 +20,17 @@ const { scene, camera } = useTresContext()
 const { pass } = useEffectThree(() => new RenderPixelatedPass(props.pixelSize, scene.value, camera.value!))
 
 defineExpose({ pass })
+
+watchEffect(() => {
+  pass.value.setPixelSize(props.pixelSize)
+})
+
+makePropWatchers(
+  [
+    [() => props.depthEdgeStrength, 'depthEdgeStrength'],
+    [() => props.normalEdgeStrength, 'normalEdgeStrength'],
+  ],
+  pass,
+  () => new RenderPixelatedPass(1, scene.value, camera.value!),
+)
 </script>
