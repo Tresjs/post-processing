cf6e56ad87791e4a173675a3be5ab40b379a3323
Tino Koch <17991193+Tinoooo@users.noreply.github.com>
2024-09-08 13:31:24 +0200

    eslint stuff

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,6 @@ export default tresLintConfig({
   ignores: ['dist', 'node_modules', 'public', '.github', 'docs/blog'],
 }, {
   rules: {
-    'perfectionist/sort-named-exports': [
-      'error',
-      {
-        type: 'line-length',
-        order: 'asc',
-      },
-    ],
     'style/max-statements-per-line': 'off',
     'jsdoc/check-alignment': 'off',
     'vue/valid-template-root': 'off',
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
+// TODO #122 seperate exports differently
+import { useEffect } from './core/composables/effect'
+import { useEffectThree } from './core/composables/useEffectThree'
 import Bloom from './core/postprocessing/Bloom.vue'
+import DepthOfField from './core/postprocessing/DepthOfField.vue'
+import EffectComposer from './core/postprocessing/EffectComposer.vue'
 import Glitch from './core/postprocessing/Glitch.vue'
 import Noise from './core/postprocessing/Noise.vue'
 import Outline from './core/postprocessing/Outline.vue'
-import Vignette from './core/postprocessing/Vignette.vue'
-import SMAAThree from './core/three/SMAA.vue' // TODO seperate exports differently
 import Pixelation from './core/postprocessing/Pixelation.vue'
-import GlitchThree from './core/three/Glitch.vue' // TODO seperate exports differently
-import OutputThree from './core/three/Output.vue' // TODO seperate exports differently
-import DepthOfField from './core/postprocessing/DepthOfField.vue'
-import HalftoneThree from './core/three/Halftone.vue' // TODO seperate exports differently
-import EffectComposer from './core/postprocessing/EffectComposer.vue'
-import PixelationThree from './core/three/Pixelation.vue' // TODO seperate exports differently
-import UnrealBloomThree from './core/three/UnrealBloom.vue' // TODO seperate exports differently
-import { useEffect } from './core/composables/effect'
-
-import { useEffectThree } from './core/composables/useEffectThree'
+import Vignette from './core/postprocessing/Vignette.vue'
 import EffectComposerThree from './core/three/EffectComposer.vue'
+import GlitchThree from './core/three/Glitch.vue'
+import HalftoneThree from './core/three/Halftone.vue'
+import OutputThree from './core/three/Output.vue'
+import PixelationThree from './core/three/Pixelation.vue'
+import SMAAThree from './core/three/SMAA.vue'
+import UnrealBloomThree from './core/three/UnrealBloom.vue'
 
 export {
   Bloom,
-  Noise,
-  Glitch,
-  Outline,
-  Vignette,
-  useEffect,
-  SMAAThree,
-  Pixelation,
-  GlitchThree,
-  OutputThree,
   DepthOfField,
-  HalftoneThree,
   EffectComposer,
-  useEffectThree,
-  PixelationThree,
-  UnrealBloomThree,
   EffectComposerThree,
+  Glitch,
+  GlitchThree,
+  HalftoneThree,
+  Noise,
+  Outline,
+  OutputThree,
+  Pixelation,
+  PixelationThree,
+  SMAAThree,
+  UnrealBloomThree,
+  useEffect,
+  useEffectThree,
+  Vignette,
 }
