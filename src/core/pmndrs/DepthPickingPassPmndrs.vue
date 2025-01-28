d77c18962d397fc28b76d1d3b1103370057c953f
alvarosabu <alvaro.saburido@gmail.com>
2025-01-28 15:05:27 +0100

    feat: improve DepthPickingPass with shallowRef and props documentation

--- src/core/pmndrs/DepthPickingPassPmndrs.vue
+++ src/core/pmndrs/DepthPickingPassPmndrs.vue
@@ -1,23 +1,33 @@
 <script lang="ts" setup>
 import type { DepthPackingStrategies } from 'three'
+import type { DepthCopyMode } from 'postprocessing'
 import { DepthPickingPass } from 'postprocessing'
-import { inject, nextTick, onUnmounted, watchEffect } from 'vue'
+import { inject, nextTick, onUnmounted, shallowRef, watchEffect } from 'vue'
 import { effectComposerInjectionKey } from './EffectComposerPmndrs.vue'
 
 export interface DepthPickingPassPmndrsProps {
+  /**
+   * The depth packing strategy.
+   * Default: RGBADepthPacking
+   */
   depthPacking?: DepthPackingStrategies
-  mode?: number
+
+  /**
+   * The depth copy mode.
+   * Default: DepthCopyMode.SINGLE
+   */
+  mode?: DepthCopyMode
 }
 
 const props = defineProps<DepthPickingPassPmndrsProps>()
 const composer = inject(effectComposerInjectionKey)
 
-const pass = new DepthPickingPass(props)
+const pass = shallowRef(new DepthPickingPass(props))
 
 const unwatch = watchEffect(() => {
   if (!composer?.value) { return }
   nextTick(() => unwatch())
-  composer.value.addPass(pass)
+  composer.value.addPass(pass.value)
 })
 
 onUnmounted(() => {
