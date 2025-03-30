dbc10833cb364f6349664eab9dca3fc3523ba5ff
Damien Montastier <montastier.damien@gmail.com>
2025-03-30 17:43:53 +0200

    init smaa effect source

---
+++ src/core/pmndrs/SMAAPmndrs.vue
@@ -0,0 +1,130 @@
+<script lang="ts" setup>
+import type { BlendFunction, EdgeDetectionMode, PredicationMode, SMAAPreset } from 'postprocessing'
+import { EffectPass, SMAAEffect, TextureEffect } from 'postprocessing'
+import { makePropWatchers } from '../../util/prop'
+import { useEffectPmndrs } from './composables/useEffectPmndrs'
+import { inject, watch } from 'vue'
+import { useTresContext } from '@tresjs/core'
+import { effectComposerInjectionKey } from './EffectComposerPmndrs.vue'
+
+export interface SMAAPmndrsProps {
+  blendFunction?: BlendFunction
+  opacity?: number
+  preset?: SMAAPreset
+  edgeDetectionMode?: EdgeDetectionMode
+  predicationMode?: PredicationMode
+  debug?: number // 0: normal, 1: edges, 2: weights
+}
+
+const props = defineProps<SMAAPmndrsProps>()
+
+const { pass, effect } = useEffectPmndrs(() => new SMAAEffect(props), props)
+const { camera } = useTresContext()
+const composer = inject(effectComposerInjectionKey)
+
+defineExpose({ pass, effect })
+
+makePropWatchers(
+  [
+    [() => Number(props.blendFunction), 'blendMode.blendFunction'],
+    [() => Number(props.predicationMode), 'edgeDetectionMaterial.predicationMode'],
+    [() => Number(props.edgeDetectionMode), 'edgeDetectionMaterial.edgeDetectionMode'],
+  ],
+  effect,
+  () => new SMAAEffect(),
+)
+
+watch(
+  [effect, () => props.opacity],
+  () => {
+    if (!effect.value) { return }
+
+    if (props.opacity !== undefined) {
+      effect.value.blendMode.setOpacity(props.opacity)
+    }
+    else {
+      const plainEffect = new SMAAEffect()
+      effect.value.blendMode.setOpacity(plainEffect.blendMode.getOpacity())
+      plainEffect.dispose()
+    }
+  },
+  { immediate: true },
+)
+
+watch(
+  [effect, () => props.preset],
+  () => {
+    if (!effect.value) { return }
+    effect.value.applyPreset(Number(props.preset))
+  },
+)
+
+let smaaEdgesDebugPass: EffectPass | null = null
+let smaaWeightsDebugPass: EffectPass | null = null
+
+const createDebugPass = (type: 'edges' | 'weights') => {
+  if (!effect.value) { return null }
+
+  const texture = type === 'edges' ? effect.value.edgesTexture : effect.value.weightsTexture
+  const pass = new EffectPass(camera.value, effect.value, new TextureEffect({ texture }))
+  pass.renderToScreen = false
+  pass.enabled = false
+  pass.fullscreenMaterial.encodeOutput = false
+
+  return pass
+}
+
+const ensureDebugPass = (type: 'edges' | 'weights') => {
+  if (type === 'edges' && !smaaEdgesDebugPass) {
+    smaaEdgesDebugPass = createDebugPass('edges')
+  }
+  if (type === 'weights' && !smaaWeightsDebugPass) {
+    smaaWeightsDebugPass = createDebugPass('weights')
+  }
+}
+
+const manageDebugPass = (pass: EffectPass | null, active: boolean) => {
+  if (!pass || !composer?.value) { return }
+
+  pass.enabled = active
+  pass.renderToScreen = active
+
+  if (active && !composer?.value.passes.includes(pass)) {
+    composer?.value.addPass(pass)
+  }
+  else if (!active && composer?.value.passes.includes(pass)) {
+    composer?.value.removePass(pass)
+    pass.dispose()
+  }
+}
+
+const updateDebugMode = (mode: number) => {
+  if (!pass.value) { return }
+
+  const mainActive = mode === 0
+  const edgesActive = mode === 1
+  const weightsActive = mode === 2
+
+  pass.value.enabled = mainActive
+  pass.value.renderToScreen = mainActive
+
+  if (edgesActive) { ensureDebugPass('edges') }
+  if (weightsActive) { ensureDebugPass('weights') }
+
+  manageDebugPass(smaaEdgesDebugPass, edgesActive)
+  manageDebugPass(smaaWeightsDebugPass, weightsActive)
+
+  if (!edgesActive) { smaaEdgesDebugPass = null }
+  if (!weightsActive) { smaaWeightsDebugPass = null }
+}
+
+watch(
+  () => props.debug,
+  () => {
+    if (!pass.value || props.debug === undefined) { return }
+
+    updateDebugMode(props.debug)
+  },
+  { immediate: true },
+)
+</script>
